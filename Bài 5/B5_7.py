a90dfc4ccff8fa0fc3e0879ae1e31a1b349db366
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-12-13 15:54:07 +0700

    small optimization

--- Bài 5/B5_7.py
+++ Bài 5/B5_7.py
@@ -1,7 +1,7 @@
-import math as mt
+from math import sqrt
 a = float(input("Nhập a: "))
 b = float(input("Nhập b: "))
 c = float(input("Nhập c: "))
 p = a+b+c
 print("Chu vi:",p)
-print("Diện tích:",round(mt.sqrt(p*(p-a)*(p-b)*(p-c)),2))
+print("Diện tích:",round(sqrt(p*(p-a)*(p-b)*(p-c)),2))
--- Bài 6/B6_5.py
+++ Bài 6/B6_5.py
@@ -1,8 +1,8 @@
-import re
+import re # Regex import
 inp = ""
 while (len(inp) != 1):
     inp = input("Nhập 1 kí tự: ")
-    if (len(inp) > 1): print("Lỗi. Nhập lại.")
+    if (len(inp) > 1): print("Lỗi. Nhập lại.") # Optimal
 # regex = r"[a-zA-Z]"
 # res = bool(re.search(regex,inp))
 # print("Kí tự nằm trong [a-Z]" if res else "Kí tự không nằm trong [a-Z]")
