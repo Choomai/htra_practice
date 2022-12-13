2b6df64f51abf1e2e36fecc190fad1fb8c453b57
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-12-13 15:41:05 +0700

    code optmization

--- SGK_p96_ex1.py
+++ SGK_p96_ex1.py
@@ -1,8 +1,15 @@
+class timeCalc:
+    def __init__(self, inp, type):
+        if type == 0: type = 86400
+        elif type == 1: type = 3600
+        else: type = 60
+        self.rem = inp % type
+        self.calc = inp // type
 raw_secs = int(input("Nhập giây: "))
-d = raw_secs // 86400
-d_left = raw_secs % 86400
-h = d_left // 3600
-h_left = d_left % 3600
-m = h_left // 60
-s = h_left % 60
-print(d,"d ",h,"h ",m,"m ",s,"s ",sep="") 
+d = timeCalc(raw_secs,0).calc
+d_rem = timeCalc(raw_secs,0).rem
+h = timeCalc(d_rem,1).calc
+h_rem = timeCalc(d_rem,1).rem
+m = timeCalc(h_rem,2).calc
+s = timeCalc(h_rem,2).rem
+print(f"{d}d {h}h {m}m {s}s",end="")
