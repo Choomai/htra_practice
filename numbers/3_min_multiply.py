8ce1cf3d5ec442c31ac8f2c5e832ada5da2ae4c3
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-07-27 19:26:52 +0700

    Need comment

--- numbers/3_min_multiply.py
+++ numbers/3_min_multiply.py
@@ -5,7 +5,7 @@ for _ in range(t): nums.append([int(elem) for elem in input().split()])
 for arr in nums:
     A, B, X, Y, N = arr
     min_product = float('inf') # Get an infinity float number.
-    for a_used in range(2):
+    for a_used in [0, N]:
         b_used = min(N - a_used, B - Y)
         a = max(A - (N - b_used), X)
         b = B - b_used
