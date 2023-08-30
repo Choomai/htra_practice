eb43bffc3a8e6674f4e4623b355f1454a76c199a
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-08-30 11:25:50 +0700

    Problem 3 solved

--- numbers/B3_giftcode.py
+++ numbers/B3_giftcode.py
@@ -1,9 +1,12 @@
-import sys
-sys.path.append("..")
-from null_libs import *
 import re
 
 n = int(input())
-inp, result = [], []
-for _ in range(n): inp.append(input())
+inp = {}
+for _ in range(n): inp[input()] = 0
 
+for code in inp.keys():
+    inp[code] = len(re.findall("[0-9]", code))
+result = dict(sorted(inp.items(), key=lambda item: item[1]))
+
+print("===============")
+for code in result.keys(): print(code)
