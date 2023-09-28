ddae6f091698fd0de8f850db7f1a3d82d7436f11
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-09-29 01:44:08 +0700

    Reverse your ass

---
+++ realtest/TTH 18-19/3.py
@@ -0,0 +1,13 @@
+input() # N is useless
+candy_packs = sorted([int(pack) for pack in input().split()], reverse=True)
+group_1, group_2 = [],[]
+sum_1, sum_2 = 0,0
+for pack in candy_packs:
+    if sum_1 < sum_2:
+        group_1.append(pack)
+        sum_1 += pack
+    else:
+        group_2.append(pack)
+        sum_2 += pack
+
+print(abs(sum_1 - sum_2), group_1, group_2, sep="\n")
