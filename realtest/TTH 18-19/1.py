62739e8cd5c612616b2d8c8891c394195d3c58e5
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-09-29 12:40:20 +0700

    Fixed now, comment not there

--- realtest/TTH 18-19/1.py
+++ realtest/TTH 18-19/1.py
@@ -10,7 +10,7 @@ def calc(dist_arr: list, csm_arr: list) -> list:
     for index in range(len(dist_arr)): # or csm_arr
         tmp = dist_arr[index] * csm_arr[index]
         result += tmp
-        indexes.append(str(consume_rates.index(csm_arr[index]) + 1))
+        indexes.insert(distances.index(dist_arr[index]), str(consume_rates.index(csm_arr[index]) + 1))
     
     return [result, indexes]
 
@@ -18,7 +18,7 @@ if len(distances) == len(consume_rates):
     print(calc(sorted_dist, sorted_csm_rt))
     exit(0)
 
-for _ in range(m - n): consume_rates.remove(sorted_csm_rt.pop())
+for _ in range(m - n): sorted_csm_rt.pop()
 final_result = calc(sorted_dist, sorted_csm_rt)
 print(final_result[0])
 print("\n".join(final_result[1]))
