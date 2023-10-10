ec2e165cf87ced05bfa0fca427223c22138587df
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-10-10 23:39:11 +0700

    Done, can sleep now

--- realtest/TTH 19-20/4.py
+++ realtest/TTH 19-20/4.py
@@ -1,19 +1,12 @@
-def isPrime(n):
-    if n <= 1: return False
-    if n <= 3: return True
-    if n % 2 == 0 or n % 3 == 0: return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0: return False
-        i += 6
-    return True
+from sys import path
+path.append("../../")
+from null_libs import isPrime
 
-# N = int(input())
-N = 10000
+N = int(input())
 result = [N]
 current = N - 1
 while current > 1:
     if isPrime(current) or any(current % num == 0 for num in result):
         result.append(current)
     current -= 1
-print(result)
+print(" ".join(map(str, result)), len(result), sep="\n")
