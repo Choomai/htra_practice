1f833de804e3e6e84f68fb0b71c3b3883f6bf266
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-10-10 23:27:55 +0700

    copy without checking is dangerous..

--- null_libs/__init__.py
+++ null_libs/__init__.py
@@ -21,12 +21,12 @@ def FParser(content: str,mode: str, convFloat=True):
     else: raise NotImplementedError("Missing parameters or mode not found")
 
 def isPrime(inp: int) -> bool:
-    if n <= 1: return False
-    if n <= 3: return True
-    if n % 2 == 0 or n % 3 == 0: return False
+    if inp <= 1: return False
+    if inp <= 3: return True
+    if inp % 2 == 0 or inp % 3 == 0: return False
     i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0: return False
+    while i * i <= inp:
+        if inp % i == 0 or inp % (i + 2) == 0: return False
         i += 6
     return True
 
