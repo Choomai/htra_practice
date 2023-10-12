3cbe99853bd4e92e68dc2bb2614cf6a46ba9ac8a
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-10-12 10:08:23 +0700

    100ms for the test, good enough i guess

--- realtest/TTH 17-18/1.py
+++ realtest/TTH 17-18/1.py
@@ -1,1 +1,18 @@
+from itertools import permutations
+import re
+
+
 n = int(input())
+bricks_arr = []
+for _ in range(n): bricks_arr.append(input())
+possible_solutions = permutations(bricks_arr, n)
+
+result = 0
+
+for solution in possible_solutions:
+    solution_str = re.findall(r"[0-9]+", "".join(solution))
+    found_str = max(solution_str, key=len)
+    found_sum = sum([int(num) for num in list(found_str)])
+    if found_sum > result: result = found_sum
+
+print(result)
