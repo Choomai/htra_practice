645983f6843fdd737f4d42741e08bfc53bdb739a
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-10-16 10:35:19 +0700

    Okay clean and easy to read i guess

--- realtest/TTH 12-13/2.py
+++ realtest/TTH 12-13/2.py
@@ -1,13 +1,14 @@
 from itertools import product
 
-def haveSubstr(inp) -> bool:
+def isUnique(inp) -> bool:
     inp = "".join( map(str, list(inp)) ) # Use for itertools.product input.
     n = len(inp)
     for length in range(1, n // 2 + 1):  # length of substring
         for i in range(n - 2 * length + 1):  # start index of first substring
-            if inp[i:i+length] == inp[i+length:i+2*length]: return True
-    return False
+            if inp[i:i+length] == inp[i+length:i+2*length]: return False
+    return True
 
 counter = 1
 n, m = [int(elem) for elem in input().split()]
-print("\n".join(map(lambda inp: "".join(map(str, inp)), filter(lambda inp: "".join(map(str, inp)), filter(haveSubstr, product(range(1, m + 1), repeat=n))))))
+for valid_str in filter(isUnique, product(range(1, m + 1), repeat=n)):
+    print("".join( map(str, valid_str) ))
