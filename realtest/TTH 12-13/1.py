e7e0e680200ee18115b59bdf17109545330f38df
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-10-16 09:51:19 +0700

    its done i guess

--- realtest/TTH 12-13/1.py
+++ realtest/TTH 12-13/1.py
@@ -1,9 +1,21 @@
+io = {
+    "in": open("io/1_GIAITHUA.inp", "r"),
+    "out": open("io/1_GIAITHUA.out", "w")
+}
 fact_preCalc = [1, 1, 2, 6, 4, 2, 2, 4, 2, 8]
 
 def fact_non0LastDigit(n):
     if n < 10: return fact_preCalc[n]
 
     if int(str(n)[-2]) % 2 == 0:
-        return (4 * fact_non0LastDigit(n // 5) * fact_non0LastDigit(int(str(n)[-1])))
+        equation = 4 * fact_non0LastDigit(n // 5) * fact_non0LastDigit(int(str(n)[-1]))
+        return int(str(equation)[-1])
     else:
-        return (6 * fact_non0LastDigit(n // 5) * fact_non0LastDigit(int(str(n)[-1])))
+        equation = 6 * fact_non0LastDigit(n // 5) * fact_non0LastDigit(int(str(n)[-1]))
+        return int(str(equation)[-1])
+    
+for num in io["in"]:
+    io["out"].write(str(fact_non0LastDigit(int(num))) + "\n")
+
+io["in"].close()
+io["out"].close()
