48b72f2df01d4910c0dd0b9d21b6ee99e82fcfaf
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-10-18 23:49:24 +0700

    Print not found class how?

--- realtest/TTH 12-13/3.py
+++ realtest/TTH 12-13/3.py
@@ -7,25 +7,27 @@ sorted_comp_rooms = sorted(comp_rooms, reverse=True)
 
 found_counter = 0
 found_rooms = []
-visited = [False for _ in range(n)]
+visited = [False for _ in range(m)]
 
 for cls in students_cls:
-    max_diff, max_index = float('inf'), -1
     for comp in sorted_comp_rooms:
+        max_diff, max_index = float('inf'), -1
         if (comp >= cls + 1) and (max_diff > comp - cls - 1):
             start_at = 0
-            while start_at < n - 1:
+            while start_at < n:
                 tmp_index = comp_rooms.index(comp, start_at)
                 if visited[tmp_index]: start_at += 1
                 else:
                     max_index = tmp_index
+                    visited[tmp_index] = True
                     break
     
-    if max_index != -1:
-        found_counter += 1
-        found_rooms.append(max_index + 1)
-        need_remove = comp_rooms[max_index]
-        sorted_comp_rooms.remove(need_remove)
+        if max_index != -1:
+            found_counter += 1
+            found_rooms.append(max_index + 1)
+            need_remove = comp_rooms[max_index]
+            sorted_comp_rooms.remove(need_remove)
+            break
 
 print("==================")
 print(found_counter)
