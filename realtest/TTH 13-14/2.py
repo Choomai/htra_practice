0df2fdddb1d1a99d939142505732d8ee4fef1c7a
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-10-19 00:11:05 +0700

    good idea but there is something wrong

--- realtest/TTH 13-14/2.py
+++ realtest/TTH 13-14/2.py
@@ -1,6 +1,20 @@
-base26 = [None, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+base26 = {i: char for i, char in enumerate("abcdefghijklmnopqrstuvwxyz")}
 
-def translator(big_num: int) -> str:
-    nums = []
-    while big_num >= 26:
-        # try: nums[]
+def int2str(integer):
+    array = []
+    while integer:
+        integer, value = divmod(integer, 26)
+        array.append(base26[value - 1])
+    return ''.join(reversed(array))
+
+print(int2str(int(input())))
+# Create a value-to-symbol table.
+
+# Take a integer and base to convert to.
+# Create an array to store the digits in.
+# While the integer is not zero:
+#     Divide the integer by the base to:
+#         (1) Find the "last" digit in your number (value).
+#         (2) Store remaining number not "chopped" (integer).
+#     Save the digit in your storage array.
+# Return your joined digits after putting them in the right order.
