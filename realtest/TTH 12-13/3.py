e036ba00e0d9d4487fbbb7ca24b4ecb4f4c22436
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-10-18 23:38:22 +0700

    Fix finding index of found rooms

--- realtest/TTH 12-13/3.py
+++ realtest/TTH 12-13/3.py
@@ -7,17 +7,24 @@ sorted_comp_rooms = sorted(comp_rooms, reverse=True)
 
 found_counter = 0
 found_rooms = []
+visited = [False for _ in range(n)]
 
 for cls in students_cls:
     max_diff, max_index = float('inf'), -1
     for comp in sorted_comp_rooms:
-        if (comp >= cls + 1) and (max_diff > comp - cls - 1): max_index = comp_rooms.index(comp)
+        if (comp >= cls + 1) and (max_diff > comp - cls - 1):
+            start_at = 0
+            while start_at < n - 1:
+                tmp_index = comp_rooms.index(comp, start_at)
+                if visited[tmp_index]: start_at += 1
+                else:
+                    max_index = tmp_index
+                    break
     
     if max_index != -1:
         found_counter += 1
         found_rooms.append(max_index + 1)
         need_remove = comp_rooms[max_index]
-        comp_rooms.remove(need_remove)
         sorted_comp_rooms.remove(need_remove)
 
 print("==================")
---
+++ realtest/TTH 13-14/2.py
@@ -0,0 +1,6 @@
+base26 = [None, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def translator(big_num: int) -> str:
+    nums = []
+    while big_num >= 26:
+        # try: nums[]
