515d34b298faa67bccec52a36329dc00684aef0a
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-01-11 15:36:17 +0700

    Ok

---
+++ realtest/TTH 23-24/1.py
@@ -0,0 +1,9 @@
+n = 15
+for i in range(2, n*n):
+    divs_counter = 2
+    for j in range(2, i // 2 + 1):
+        if i % j == 0: divs_counter += 1
+
+    if divs_counter == n:
+        print(i)
+        exit(0)
