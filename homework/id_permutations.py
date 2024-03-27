3cfeb517ea7e0e7b876cd7081ebc54a22d20bca1
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-03-27 19:45:17 +0700

    too ez

--- homework/id_permutations.py
+++ homework/id_permutations.py
@@ -1,8 +1,13 @@
 from itertools import permutations
 
-mut_inp = input().split()
+mut_inp = "".join(input().split())
 id = int(input())
 n = len(mut_inp)
 
-permutations_list = list(map(lambda mut: "".join(map(str, mut)), permutations(range(n), n)))
-print(permutations_list)
+permutations_list = list(map(
+    lambda mut: "".join(map(str, mut)), # Convert (3,2,1) to "321"
+    permutations(range(1, n+1), n)
+))
+
+print(permutations_list.index(mut_inp) + 1)
+print(permutations_list[id+1])
