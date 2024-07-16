bc2934b538d3a90455844ca719dfb458021fc2e6
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-07-16 08:45:07 +0700

    bruh

--- greedy/5_ROOM.py
+++ greedy/5_ROOM.py
@@ -1,4 +1,43 @@
+from dataclasses import dataclass
+
+tests = []
+indexes = []
+
+@dataclass
+class Test:
+    start: int
+    end: int
+    index: int
+
+    def __eq__(self, other: object) -> bool:
+        return self.end == other.end
+    def __ne__(self, other: object) -> bool:
+        return self.end != other.end
+    def __lt__(self, other: object) -> bool:
+        if self.end == other.end: return self.start < other.start
+        else: return self.end < other.end
+    def __gt__(self, other: object) -> bool:
+        if self.end == other.end: return self.start > other.start
+        else: return self.end > other.end
+    def __le__(self, other: object) -> bool:
+        if self.end == other.end: return self.start <= other.start
+        else: return self.end <= other.end
+    def __ge__(self, other: object) -> bool:
+        if self.end == other.end: return self.start >= other.start
+        else: return self.end >= other.end
 
 with open("io/5_ROOM.inp", "r") as f:
     N = int(f.readline())
-    for _ in range(N):
+    for i in range(N): tests.append(Test(*map(int, f.readline().split(" ")), i))
+
+tests = sorted(tests)
+indexes = []
+last_test = -1
+for test in tests:
+    if last_test >= test.start: continue
+    last_test = test.end
+    indexes.append(str(test.index + 1))
+
+with open("io/5_ROOM.out", "w") as f:
+    f.write(f"{len(indexes)}\n")
+    f.writelines(" ".join(indexes))
