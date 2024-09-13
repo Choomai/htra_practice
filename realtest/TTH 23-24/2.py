72f8dc61bf3ba8fe9e3e649767b8e817a804c862
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-13 15:49:07 +0700

    got it

--- realtest/TTH 23-24/2.py
+++ realtest/TTH 23-24/2.py
@@ -1,21 +1,11 @@
-N = int(5)  # Giới hạn N
-a = [1] * (N + 1)
+def divisor_counts(n):
+    divisor_counts = [1] * n
+    divisor_counts[0] = None
+    for i in range(2, n):
+        for j in range(i, n, i):
+            divisor_counts[j] += 1
+    return divisor_counts
 
-
-def sang(n):
-    for i in range(1, int(n**0.5) + 1):
-        for j in range(2 * i, n + 1, i):
-            a[j] += 1
-
-# Thực hiện sàng
-sang(N)
-
-# Đọc vào giá trị n
-n = 3
-
-# Tìm số đầu tiên có đúng n ước
-for i in range(1, N + 1):
-    if a[i] == n:
-        print(i)
-        print(a[1:100])
-        break
+# Example usage:
+n = int(100)
+print(divisor_counts(n))
