d195923a946e9e3e18bbd9f0314a0508a7def491
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-13 14:24:38 +0700

    Don't know that can cause shallow copy

--- realtest/TTH 20-21/1.py
+++ realtest/TTH 20-21/1.py
@@ -2,12 +2,21 @@
 # If the current item is not too heavy, check what gives the highest value: adding it, or not adding it. Store the maximum of these two values in the table.
 # In case the current item is too heavy to be added, just use the previously calculated value at the current capacity where the current item was not considered.
 
+from pprint import pprint
+
 weights, values = [], []
-with open("realtest/TTH 20-21/io/BAG.inp", "r") as f:
+with open("io/BAG.inp", "r") as f:
     N, W = map(int, f.readline().split())
     for _ in range(N):
         weight, value = map(int, f.readline().split())
         weights.append(weight)
         values.append(value)
 
-dp = (N + 1) * [(W + 1) * [0]] # 2D array, N+1 columns, W+1 rows, 0 filled
+dp = [[0 for _ in range(W+1)] for _ in range(N+1)]
+for i in range(1, N+1):
+    for w in range(1, W+1):
+        if weights[i-1] > w:
+            dp[i][w] = dp[i-1][w]
+        else: 
+            dp[i][w] = max(dp[i-1][w], dp[i-1][w - weights[i-1]] + values[i-1])
+pprint(dp)
