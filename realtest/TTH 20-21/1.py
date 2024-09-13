1b2684c815a775f380cd478f9ece1582c00f81c7
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-13 14:50:27 +0700

    Hmmm

--- realtest/TTH 20-21/1.py
+++ realtest/TTH 20-21/1.py
@@ -1,22 +1,27 @@
-# Consider one item at a time, and increase the knapsack capacity from 0 to the knapsack limit.
-# If the current item is not too heavy, check what gives the highest value: adding it, or not adding it. Store the maximum of these two values in the table.
-# In case the current item is too heavy to be added, just use the previously calculated value at the current capacity where the current item was not considered.
-
-from pprint import pprint
-
-weights, values = [], []
+weights, values = [None], [None] # 1-based indexes
 with open("io/BAG.inp", "r") as f:
     N, W = map(int, f.readline().split())
     for _ in range(N):
-        weight, value = map(int, f.readline().split())
-        weights.append(weight)
-        values.append(value)
+        tmp = list(map(int, f.readline().split()))
+        weights.append(tmp[0])
+        values.append(tmp[1])
+    del tmp
 
 dp = [[0 for _ in range(W+1)] for _ in range(N+1)]
 for i in range(1, N+1):
     for w in range(1, W+1):
-        if weights[i-1] > w:
+        if weights[i] > w:
             dp[i][w] = dp[i-1][w]
         else: 
-            dp[i][w] = max(dp[i-1][w], dp[i-1][w - weights[i-1]] + values[i-1])
-pprint(dp)
+            dp[i][w] = max(dp[i-1][w], dp[i-1][w - weights[i]] + values[i])
+    
+print(dp[N][W])
+
+selected_items = []
+i, j = N, W
+while i > 1 and j > 1:
+    if dp[i][j] != dp[i-1][j]:
+        selected_items.append(i)
+        j -= weights[i-1]
+    i -= 1
+print(*selected_items)
