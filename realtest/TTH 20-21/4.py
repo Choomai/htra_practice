e5f8943f436468efe0f691ca1e9197d7660a387a
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-16 14:40:40 +0700

    Recursion caching

--- realtest/TTH 20-21/4.py
+++ realtest/TTH 20-21/4.py
@@ -1,9 +1,15 @@
+from functools import lru_cache
+
 with open("io/BUILD.inp", "r") as f:
     n = int(f.readline())    
     tests = list(map(int, f.read().split()))
 
-cache = []
+@lru_cache(maxsize=1024)
 def fib(n):
-    if n == 1: return 0
-    elif n == 2: return 1
-    
+    if n == 0: return 0
+    elif n == 1: return 1
+
+    return fib(n-1) + fib(n-2)
+
+for test in tests:
+    print(fib(test+1))
