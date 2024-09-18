1589d2edf86ceeb618d112c679d6a412d71ae62b
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-18 07:48:49 +0700

    optimize

--- realtest/TTH 22-23/3.py
+++ realtest/TTH 22-23/3.py
@@ -1,13 +1,9 @@
-inp = open("io/3_MESS.INP", "r")
-n = inp.readline().split(" ")[0]
-raw_lst = inp.read().split("\n")
-cnt,lst = 0,[]
-for raw in raw_lst:
-    temp = raw.split(" ")
-    lst.append(int(temp[1]))
+with open("io/3_MESS.INP", "r") as f:
+    n = f.readline().split()[0]
+    lst = [int(k.split()[1]) for k in f.read().split("\n")]
+
+cnt = 0
 for i in range(1,int(n)+1):
     if not i in lst: cnt += 1
-out = open("io/3_MESS.OUT", "w")
-out.write(str(cnt))
-inp.close()
-out.close()
+
+with open("io/3_MESS.OUT", "w") as f: f.write(str(cnt))
