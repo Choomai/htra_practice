2e1cde455a847fc6648dfefcbfd8044ea284cfeb
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-18 11:16:49 +0700

    optimus prime

--- realtest/TTH 22-23/4.py
+++ realtest/TTH 22-23/4.py
@@ -3,6 +3,6 @@ from string import ascii_lowercase
 
 t = int(input())
 grp_work = []
-for _ in range(t): grp_work.append([int(elem) for elem in input().split()])
+for _ in range(t): grp_work.append(tuple(map(int, input().split())))
 
 for k, n in grp_work: print(len(list(combinations(ascii_lowercase[:k], n))))
