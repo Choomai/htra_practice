6fe45b6b0d118598aa2d2c474995e80bb57d8f3b
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-20 10:03:19 +0700

    hmmm

--- realtest/TTH 21-22/2.py
+++ realtest/TTH 21-22/2.py
@@ -20,12 +20,20 @@ with open("io/ENEGRY.inp", "r") as f:
     n, m = map(int, f.readline().split())
     distances = list(map(int, f.readline().split()))
     consume_rates = []
+    org_consume_rates = []
     for index, consume_rate in enumerate(map(int, f.readline().split())):
         consume_rates.append(Car(consume_rate, -1, index))
+        org_consume_rates.append(consume_rate)
 
 consume_rates = sorted(consume_rates)
 sorted_distances = sorted(distances)
 chosen_car = []
 
-for distance in distances:
+for index, distance in enumerate(sorted_distances):
+    consume_rates[index].distance = distance
+    consume_rate_index = org_consume_rates.index(consume_rates[index].consume_rate)
     
+    chosen_car.append(consume_rate_index+1)
+    org_consume_rates[consume_rate_index] = -1
+
+pprint(chosen_car)
--- realtest/TTH 22-23/1.py
+++ realtest/TTH 22-23/1.py
@@ -1,9 +1,13 @@
 with open("io/1_TIMX.INP", "r") as f:
     lst = f.read().split(" ")
-    lst = [int(elem) for elem in lst]
+    a1, b1, a2, b2 = [int(elem) for elem in lst]
 i,j = 0,0
-while True:
-    j = ((lst[0] * i + lst[1]) - lst[3]) / lst[2]
+
+special_case = False
+if a2 == 0: 
+
+while not special_case:
+    j = ((a1 * i + b1) - b2) / a2
     if j == int(j): break
     i += 1
 with open("io/1_TIMX.OUT", "w") as f:
