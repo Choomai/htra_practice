e37fd703e7b70377221249dd73fd54f482bca4d1
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-20 10:03:41 +0700

    removed old method

--- unknown/rent.py
+++ unknown/rent.py
@@ -12,32 +12,6 @@ with open("io/rent.inp", "r") as f:
     N = int(f.readline())
     customers = [Customer(*map(int, f.readline().split()), i) for i in range(N)]
 
-# def check(selected: combinations) -> bool:
-#     if len(selected) == 1: return True
-
-#     selected = list(selected)
-#     selected.sort(key=lambda x: x.start_time)
-
-#     for i in range(0, len(selected)-1):
-#         if selected[i].end_time > selected[i+1].start_time: return False
-#     return selected
-
-# max_income = 0
-# selected_comb = None
-# for i in range(N, 1, -1): # N -> 2
-#     for comb in combinations(customers, i):
-#         current_income = sum((customer.pay for customer in comb))
-#         if current_income <= max_income: continue
-
-#         passed_comb = check(comb)
-#         if not passed_comb: continue
-#         max_income = current_income
-#         selected_comb = passed_comb
-
-
-# print(len(selected_comb), max_income)
-# print(*(comb.index+1 for comb in selected_comb))
-
 customers.sort(key=lambda x: x.start_time)
 dp = [{"income": 0, "indexes": []} for _ in range(N+1)]
 
