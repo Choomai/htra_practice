258f5714be85654f8b1c1c99d864a74a93378fb7
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-26 16:15:03 +0700

    not 100%

--- numbers/C9_sq.py
+++ numbers/C9_sq.py
@@ -6,7 +6,7 @@ def check(inp: int) -> bool:
 
 N = int(input())
 squares = tuple(filter(check, range(1,N)))
-for i in range(2, len(squares)):
+for i in range(2, len(squares)+1):
     for comb in combinations_with_replacement(squares, i):
         if sum(comb) == N:
             print(i)
