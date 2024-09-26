be74ed70b977dbbd453e74107db8ba28a964c65b
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-09-26 09:12:25 +0000

    pussh

---
+++ numbers/C9_sq.py
@@ -0,0 +1,13 @@
+from math import sqrt
+from itertools import combinations_with_replacement
+
+def check(inp: int) -> bool:
+    return int(sqrt(inp))**2 == inp
+
+N = int(input())
+squares = tuple(filter(check, range(1,N)))
+for i in range(2, len(squares)):
+    for comb in combinations_with_replacement(squares, i):
+        if sum(comb) == N:
+            print(i)
+            exit(0)
