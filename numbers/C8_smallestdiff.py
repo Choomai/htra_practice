b95eaee5b087fbda12dc51182f8252e4e7d20731
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-09-26 03:08:48 +0000

    should be okay

--- numbers/C8_smallestdiff.py
+++ numbers/C8_smallestdiff.py
@@ -1,18 +1,13 @@
-from itertools import combinations
-
 with open("io/C8_smallestdiff.inp") as f:
     N = int(f.readline())
-    cards = tuple(map(int, f.readline().split()))
+    cards = list(map(int, f.readline().split()))
 
-cards_combs = combinations(cards, N//2)
 min_diff = float("inf")
 
-for cards1, cards2 in combinations(cards_combs, 2):
-    current_diff = abs(sum(cards1) - sum(cards2))
-    if current_diff < min_diff:
-        min_diff = current_diff
-    elif current_diff == 0:
-        print(0)
-        exit(0)
+cards_sum1, cards_sum2 = 0, 0
+cards.sort(reverse=True)
+for card in cards:
+    if cards_sum1 >= cards_sum2: cards_sum2 += card
+    else: cards_sum1 += card
 
-print(min_diff)
+print(abs(cards_sum1 - cards_sum2))
