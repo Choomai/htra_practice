1e275b2f43491cefad64d77b7f6349998f29eaca
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-09-28 09:20:31 +0700

    ez

---
+++ graph/2_roundtrip.py
@@ -0,0 +1,15 @@
+with open("io/2_roundtrip.inp", "r") as f:
+    N, P = map(int, f.readline().split())
+    trip = ""
+    for _ in range(P): trip += f.readline()[:3:2]
+
+indexes = [None, None]
+cached_num = None
+
+def generate_trip(max_node: int) -> str:
+    base_str = list(map(str, range(max_node)))
+    base_str.append("0")
+    for i in range(1, max_node): base_str[i] = base_str[i] * 2
+    return "".join(base_str)
+
+print(generate_trip(N) in trip)
